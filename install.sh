0e9484f12c6cc5558bfae052a905ce0c32ab3f6a
icsl-Jeon <junbs95@gmail.com>
2021-08-17 21:55:30 +0900

    starting travis

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 sudo apt-get install git
 sudo apt-get install libeigen-dev
+mkdir -p ~/chaser_ws/src
+cd ~/chaser_ws/src
+# qpOASES
 git clone https://github.com/coin-or/qpOASES.git
+cd qpOASES
+mkdir build && cd build
+cmake .. -DCMAKE_CXX_FLAGS=-fPIC
+sudo make install
+#
